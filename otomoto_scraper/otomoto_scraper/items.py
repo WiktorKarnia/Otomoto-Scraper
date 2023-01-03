e7cc566256744fac47d8daec1a725c376634cf29
WiktorKarnia <wiktor1341@o2.pl>
2023-01-03 20:11:53 +0100

    change of plans

--- otomoto_scraper/otomoto_scraper/items.py
+++ otomoto_scraper/otomoto_scraper/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class GaspriceScraperItem(scrapy.Item):
+class OtomotoScraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
--- otomoto_scraper/otomoto_scraper/settings.py
+++ otomoto_scraper/otomoto_scraper/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for gasprice_scraper project
+# Scrapy settings for otomoto_scraper project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,14 +7,14 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'gasprice_scraper'
+BOT_NAME = 'otomoto_scraper'
 
-SPIDER_MODULES = ['gasprice_scraper.spiders']
-NEWSPIDER_MODULE = 'gasprice_scraper.spiders'
+SPIDER_MODULES = ['otomoto_scraper.spiders']
+NEWSPIDER_MODULE = 'otomoto_scraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'gasprice_scraper (+http://www.yourdomain.com)'
+#USER_AGENT = 'otomoto_scraper (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -45,13 +45,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'gasprice_scraper.middlewares.GaspriceScraperSpiderMiddleware': 543,
+#    'otomoto_scraper.middlewares.OtomotoScraperSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'gasprice_scraper.middlewares.GaspriceScraperDownloaderMiddleware': 543,
+#    'otomoto_scraper.middlewares.OtomotoScraperDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'gasprice_scraper.pipelines.GaspriceScraperPipeline': 300,
+#    'otomoto_scraper.pipelines.OtomotoScraperPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
